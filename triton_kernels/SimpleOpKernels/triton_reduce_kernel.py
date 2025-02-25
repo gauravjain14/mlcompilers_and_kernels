b36d78edf8f98155280dbef7cba57fcb04d3bc58
gauravjain14 <gaurjain14@gmail.com>
2025-02-25 20:46:50 +0000

    Changing from 1D blocks to 2D reduces time by 50% but still 3x off

--- triton_kernels/SimpleOpKernels/triton_reduce_kernel.py
+++ triton_kernels/SimpleOpKernels/triton_reduce_kernel.py
@@ -4,6 +4,8 @@ import triton
 import triton.language as tl
 
 
+torch.set_printoptions(sci_mode=False)
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 @triton.jit
@@ -26,8 +28,6 @@ def matrix_sum_kernel(x_ptr, z_ptr, M, B0: tl.constexpr=32):
         row_sum += tl.sum(x)  # Accumulate sum
 
     # Store row sum in the output array
-    # using z_ptr or z_ptr + pid_0 should be the same since we are only
-    # moving along the row dimension?
     tl.store(z_ptr + pid_0, row_sum)
 
 @triton.jit
@@ -46,6 +46,53 @@ def final_reduce(x_ptr, z_ptr, M, B0: tl.constexpr=32):
 
     tl.store(z_ptr + pid_0, row_sum)
 
+@triton.jit
+def fused_sum_kernel(x_ptr, z_ptr, M, BLOCK_SIZE_M: tl.constexpr):
+    pid_0 = tl.program_id(0)  # Row index
+    num_cols_blocks = (M + BLOCK_SIZE_M - 1) // BLOCK_SIZE_M
+
+    row_start = x_ptr + pid_0 * M
+    
+    block_sum = 0.0
+    # Compute loop bounds
+    loop_max = (M + BLOCK_SIZE_M - 1) // BLOCK_SIZE_M
+    for i in range(loop_max):
+        # Now, that we know the number of rows per program is > 1, the offsets
+        # also need to be calculated accordingly.
+        offsets = i * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        mask = offsets < M
+        x = tl.load(row_start + offsets, mask=mask, other=0.0)
+        block_sum += tl.sum(x)  # Accumulate sum
+
+    tl.atomic_add(z_ptr, block_sum)
+
+
+@triton.jit
+def fused_sum_kernel_block(x_ptr, z_ptr, M, BLOCK_SIZE_M: tl.constexpr):
+    pid_0 = tl.program_id(0)  # Row index
+    num_cols_blocks = (M + BLOCK_SIZE_M - 1) // BLOCK_SIZE_M
+
+    start_row = pid_0 * BLOCK_SIZE_M
+    
+    block_sum = 0.0
+    # Compute loop bounds
+    loop_max = (M + BLOCK_SIZE_M - 1) // BLOCK_SIZE_M
+    for i in range(loop_max):
+        # Now, that we know the number of rows per program is > 1, the offsets
+        # also need to be calculated accordingly.
+        offsets_x = i * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        offsets_y = start_row + tl.arange(0, BLOCK_SIZE_M)
+
+        addr_range = offsets_y[:, None] * M + offsets_x[None, :]
+        mask_x = offsets_x < M
+        mask_y = offsets_y < M
+        mask = mask_y[:, None] & mask_x[None, :]
+        x = tl.load(x_ptr + addr_range, mask=mask, other=0.0)
+        block_sum += tl.sum(x)  # Accumulate sum
+
+    tl.atomic_add(z_ptr, block_sum)
+
+
 # let's add benchmarking here. For comparison, we will use the torch compile
 # for reduce sum and compare the performance with Triton kernel above, using
 # torch profiler
@@ -57,6 +104,8 @@ def benchmarking():
     print(f"========== Benchmarking Torch Compile ==========")
     inp_matrix = torch.randn(1000, 1000, device=device)
     matrix = inp_matrix.clone()
+
+    # torch._logging.set_logs(output_code=True)
     torch_reduce_sum = torch.compile(torch.sum)
     with torch.profiler.profile(
         activities=[torch.profiler.ProfilerActivity.CUDA],
@@ -70,18 +119,20 @@ def benchmarking():
         event_list = prof.key_averages()
         print(event_list)
 
+    # torch._logging.set_logs(output_code=False)
     # Triton kernel
     print(f"========== Benchmarking Triton Kernel ==========")
     intermediate = torch.zeros((1000,), device=device)
     output = torch.zeros((1,), device=device)
-    grid = lambda meta: (triton.cdiv(matrix.shape[0], 1),)
+    grid = lambda meta: (triton.cdiv(matrix.shape[0], 32),)
     with torch.profiler.profile(
         activities=[torch.profiler.ProfilerActivity.CUDA],
         schedule=torch.profiler.schedule(wait=5, warmup=5, active=20, repeat=1)
     ) as prof:
         for _ in range(30):
-            matrix_sum_kernel[grid](inp_matrix, intermediate, matrix.shape[1])
-            final_reduce[(1, 1, 1)](intermediate, output, intermediate.shape[0])
+            # matrix_sum_kernel[grid](inp_matrix, intermediate, matrix.shape[1])
+            # final_reduce[(1, 1, 1)](intermediate, output, intermediate.shape[0])
+            fused_sum_kernel_block[grid](matrix, output, matrix.shape[1], 32)
             torch.cuda.synchronize(device)
             prof.step()
 
@@ -93,14 +144,19 @@ def benchmarking():
 matrix = torch.randn(1000, 1000, device='cuda')
 intermediate = torch.zeros((1000,), device='cuda')
 output = torch.zeros((1,), device='cuda')
+output_fused = torch.zeros((1,), device='cuda')
 
 # Launch kernel
-grid = lambda meta: (triton.cdiv(matrix.shape[0], 1),)
-matrix_sum_kernel[grid](matrix, intermediate, matrix.shape[1])
-final_reduce[(1, 1, 1)](intermediate, output, intermediate.shape[0])
+grid = lambda meta: (triton.cdiv(matrix.shape[0], 32),)
+# matrix_sum_kernel[grid](matrix, intermediate, matrix.shape[1])
+# final_reduce[(1, 1, 1)](intermediate, output, intermediate.shape[0])
+
+fused_sum_kernel_block[grid](matrix, output_fused, matrix.shape[1], 32)
 
 ref_out = matrix.sum()
-print(f"Did the kernel match with reference - {torch.allclose(output, ref_out, atol=1e-5)}")
-
+# print(f"output {output}")
+print(f"ref_out {ref_out}")
+print(f"output fused {output_fused}")
+print(f"Did the kernel match with reference - {torch.allclose(output_fused, ref_out, atol=1e-5)}")
 # do benchmarking.
 benchmarking()
